d415520150af7d25d5f756bf39c16f06de048f71
909hehe <140858364+909hehe@users.noreply.github.com>
2023-09-15 17:03:18 +0530

    shit

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext ,useEffect, useState} from 'react';
 import React from 'react'
 import fileContext from '../context/filecontext';
 
@@ -9,10 +9,35 @@ function Add(props) {
     const context = useContext(fileContext);
 
     const {addfolder ,parent} = context;
+    const [current_rotation, setcurrent_rotation] = useState(360)
+
+    
+    const rotate = () => {
+      setcurrent_rotation(current_rotation+360);
+      console.log(current_rotation);
+      document.querySelector(".add").style.transform = 'rotate(' + current_rotation + 'deg)';
+    }
+    
+    
+    useEffect(() => {
+      
+      document.querySelector(".add").addEventListener('click', rotate);
+      
+      return () => {
+        document.querySelector(".add").removeEventListener('click', rotate);
+      }
+
+
+    }, [current_rotation])
+    
+    
+
   return (
     <>
         <div className='add' onClick={()=>{addfolder('NewFolder',parent)}}>
-            <i className="fa-solid fa-circle-plus" style={{color: `#001dfa`}}></i>
+            
+              <i className="fa-solid fa-circle-plus" style={{color : "#001dfa"}}></i>
+            
         </div>
     </>
   )
